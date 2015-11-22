622441d506e41a44405dd51309dfcf4712eb2ce8
MLanghof <max.langhof@gmx.de>
2015-11-22 17:00:56 +0100

    Initial working commit

---
+++ NeuralTrade/NeuralTrade/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace NeuralTrade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTrain = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTrain
+            // 
+            this.buttonTrain.Location = new System.Drawing.Point(230, 13);
+            this.buttonTrain.Name = "buttonTrain";
+            this.buttonTrain.Size = new System.Drawing.Size(75, 23);
+            this.buttonTrain.TabIndex = 0;
+            this.buttonTrain.Text = "Train";
+            this.buttonTrain.UseVisualStyleBackColor = true;
+            this.buttonTrain.Click += new System.EventHandler(this.buttonTrain_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 206);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(230, 42);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 444);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonTrain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTrain;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRun;
+    }
+}
+
---
+++ NeuralTrade/NeuralTrade/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using EdgeJs;
+using System.Net;
+
+namespace NeuralTrade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static async void Run()
+        {
+            var func = Edge.Func(@"
+                return function(data, callback) {
+                    callback(null, net.run(data)[0]);
+                }");
+            double[] input = new double[1];
+            input[0] = 0.1;
+
+            Console.WriteLine(await func(input));
+        }
+
+        public static async void Train()
+        {
+            var trainNetwork = Edge.Func(@"
+                var brain = require('brain');
+
+                return function (data, cb) {
+                    var net = new brain.NeuralNetwork({
+                            hiddenLayers: [2]
+                        });
+
+                    var output = net.train([
+                            {input: [0], output: [0]},
+                            {input: [0.25], output: [0.0625]},
+                            {input: [0.5], output: [0.25]},
+                            {input: [0.75], output: [0.5625]},
+                            {input: [1], output: [1]}
+                        ]);
+                    cb(null, output.error)
+                };
+            ");
+
+            List<Dictionary<string, double[]>> data = new List<Dictionary<string, double[]>>();
+
+            Console.WriteLine("Error: " + (await trainNetwork(data)));
+        }
+
+        private void buttonTrain_Click(object sender, EventArgs e)
+        {
+            Task.Run((Action)Train).Wait();
+        }
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            Task.Run((Action)Run).Wait();
+        }
+    }
+}
